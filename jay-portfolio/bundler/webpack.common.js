078b0ef5777a9ad2a7ee70367f029f03d386e434
Jay <2594jaypatel@gmail.com>
2026-03-10 12:22:53 -0400

    Fix: html-loader sources:false in jay-portfolio webpack

--- jay-portfolio/bundler/webpack.common.js
+++ jay-portfolio/bundler/webpack.common.js
@@ -36,7 +36,7 @@ module.exports = {
       // HTML
       {
         test: /\.(html)$/,
-        use: ["html-loader"],
+        use: [{ loader: "html-loader", options: { sources: false } }],
       },
 
       // JS
